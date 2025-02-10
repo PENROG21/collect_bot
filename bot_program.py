b70686f97b635c9fd6e5d15feeb2ba881d11eca9
georgijmironenko36@gmail.com <georgijmironenko36@gmail.com>
2025-02-10 20:17:03 +0300

    Добавление функций создание таблиц и запись данных пользователя в базу данных.

--- bot_program.py
+++ bot_program.py
@@ -1,12 +1,19 @@
 import telebot
 
+from data_base import PostgresConnection
+
 
 # вводим токен бота
 asd = "5929718382:AAFZXsV2YldpqY7KCW4bbwyorLwfWHZiIo0"
 bot = telebot.TeleBot(asd)
 
-
+db = PostgresConnection(
+    database="telebot",
+    password="PENROG21"
+)
+db.connect()
 user_data = {}
+
 
 # Добавление данных о пользователе
 @bot.message_handler(commands=['start'])
@@ -15,9 +22,36 @@ def send_welcome(message):
 
     markup = telebot.types.ReplyKeyboardMarkup(row_width=1)
     item1 = telebot.types.KeyboardButton("Создать таблицу")
+    item2 = telebot.types.KeyboardButton("Записаться")
     markup.add(item1)
+    markup.add(item2)
 
     bot.send_message(message.chat.id, "Выберите действие:", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: message.text.startswith('/'))
+def handle_table_link(message):
+    try:
+        table_id = message.text[1:]
+        print(table_id)
+        table_name, table_description = db.get_info_table(table_id)
+
+        if table_name:
+            response = f"Вы перешли по ссылке на таблицу с ID: {table_id}\n"
+            response += f"Название таблицы: {table_name}\n"
+            response += f"Описание таблицы: {table_description or 'Нет описания'}\n"
+            bot.reply_to(message, response)
+        else:
+            bot.reply_to(message, f"Таблица с ID {table_id} не найдена.")
+
+    except Exception as e:
+        bot.reply_to(message, f"Произошла ошибка: {e}")
+
+
+@bot.message_handler(func=lambda message: message.text == "Записаться")
+def handle_create_table(message):
+    bot.send_message(message.chat.id, "Введите id таблицы куда хотите записаться:")
+    bot.register_next_step_handler(message, lambda message: handle_table_link(message))
 
 
 @bot.message_handler(func=lambda message: message.text == "Создать таблицу")
@@ -54,23 +88,37 @@ def handle_table_name(message):
 @bot.message_handler(
     func=lambda message: user_data.get(message.chat.id, {}).get("state") == "waiting_for_table_description")
 def handle_table_description(message):
-    if message.text == "Отмена":
-        # Сбрасываем состояние и возвращаемся к началу
+    try:
+        if message.text == "Отмена":
+            # Сбрасываем состояние и возвращаемся к началу
+            user_data.pop(message.chat.id, None)
+            send_welcome(message)
+            return
+
+        # Сохраняем описание таблицы
+        user_data[message.chat.id]["table_description"] = message.text
+
+        # Выводим собранные данные
+        table_name = user_data[message.chat.id]["table_name"]
+        table_description = user_data[message.chat.id]["table_description"]
+
+        # Записываем пользователя если нет.
+        if not db.exist_user(message.from_user.id):
+            db.records_user(id_platform=1, user_id=message.from_user.id, username=message.from_user.username,
+                            user_name=message.from_user.first_name, user_surname=message.from_user.last_name)
+
+        # Создаём таблицу
+        id_table = db.create_table(table_name, table_description, id_owner=message.from_user.id)
+        # Выводим информацию об таблице
+        bot.send_message(message.chat.id, f"Таблица создана!"
+                                          f"\n ID таблицы {id_table}"
+                                          f"\nНазвание: {table_name}"
+                                          f"\nОписание: {table_description}")
+
+        # Сбрасываем состояние
         user_data.pop(message.chat.id, None)
-        send_welcome(message)
-        return
-
-    # Сохраняем описание таблицы
-    user_data[message.chat.id]["table_description"] = message.text
-
-    # Выводим собранные данные
-    table_name = user_data[message.chat.id]["table_name"]
-    table_description = user_data[message.chat.id]["table_description"]
-
-    bot.send_message(message.chat.id, f"Таблица создана!\nНазвание: {table_name}\nОписание: {table_description}")
-
-    # Сбрасываем состояние
-    user_data.pop(message.chat.id, None)
+    except Exception as error:
+        print("Ошибка при работе с функцией handle_table_description\n", error)
 
 
 # Запуск бота
--- data_base.py
+++ data_base.py
@@ -13,6 +13,14 @@ class PostgresConnection:
         self.port = port
         self.connection = None
 
+    def commit(self):
+        """Сохранение изменений в базе данных."""
+        try:
+            self.connection.commit()
+            print("Изменения сохранены.")
+        except psycopg2.Error as e:
+            print(f"Ошибка сохранения изменений: {e}")
+
     def connect(self):
         """Создание подключения к PostgreSQL."""
         try:
@@ -28,21 +36,76 @@ class PostgresConnection:
         except psycopg2.Error as e:
             print(f"Ошибка подключения: {e}")
 
+    def get_info_table(self, id_table):
+        try:
+            self.cursor.execute(f'SELECT name, discription FROM "table" WHERE id = {id_table}')
+            table_info = self.cursor.fetchone()
+
+            if table_info:
+                return table_info[0], table_info[1]
+            else:
+                return None, None
+
+        except Exception as error:
+            print("Ошибка при работе с методом get_info_table", error)
+            return None, None
+
+    def records_user(self, user_id, user_name: str, user_surname: str, username: str, id_platform: int):
+        """
+        Метод для, добавление пользователя в базу данных.
+        :param user_id: id пользователя из платформа
+        :param user_name:
+        :param user_surname:
+        :param username:
+        :param id_platform: Платформа
+        """
+        try:
+            self.cursor.execute("INSERT INTO users(id_user, user_name, username, platform, data, user_surname)"
+                                f" values ({user_id}, %s, %s, {id_platform}, now(), %s)",
+                                (user_name, username, user_surname))
+            self.commit()
+        except Exception as error:
+            print("Ошибка при работе с методом records_user\n", error)
+
+    def exist_user(self, user_id_platform: str) -> bool:
+        """
+        Метод, для проверки наличия пользователя в базе данных
+        :param user_id_platform: id пользователя из платформы
+        :return: True если есть
+        """
+        try:
+            self.cursor.execute(f"SELECT * FROM users WHERE id_user = {user_id_platform}")
+            if self.cursor.fetchone():
+                return True
+            else:
+                return False
+        except Exception as error:
+            print("Ошибка при работе с методом exist_user\n", error)
+
+    def create_table(self, name: str, description: str, id_owner: int) -> str:
+        """
+        Метод для, создание таблицы
+        :param name: Название таблицы
+        :param description: Описание таблицы
+        :param id_owner: Id создателя и владельца
+        :return:
+        """
+        try:
+            self.cursor.execute('INSERT INTO "table"(name, discription, owner, data_creat) VALUES '
+                                    f"(%s, %s, (SELECT id FROM users WHERE id_user = {id_owner}), NOW()) RETURNING id",
+                                (name, description))
+            self.commit()
+
+            return self.cursor.fetchone()[0]
+        except Exception as error:
+            print("Ошибка при работе с методом create_table\n", error)
+
     def close(self):
         """Закрытие подключения к PostgreSQL."""
         if self.connection:
             self.cursor.close()
             self.connection.close()
             print("Соединение с PostgreSQL закрыто.")
-
-
-    def commit(self):
-        """Сохранение изменений в базе данных."""
-        try:
-            self.connection.commit()
-            print("Изменения сохранены.")
-        except psycopg2.Error as e:
-            print(f"Ошибка сохранения изменений: {e}")
 
 
 if __name__ == "__main__":
